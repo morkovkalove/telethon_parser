5914b6864c4e4fbf5f7e2794859d979ce6a60697
Andrey Solovev <sir.noen@yandex.ru>
2022-09-04 14:00:43 +0300

    add new py file

---
+++ parser.py
@@ -0,0 +1,15 @@
+from telethon.sync import TelegramClient 
+from telethon.tl.functions.messages import GetHistoryRequest 
+
+api_id = 12345  #your api id
+api_hash = 'asdfghjkl123' #your api hash 
+name = 'parsing_session' #any name
+chat = 'https://t.me/...'  # Канал куда парсить
+
+
+with TelegramClient(name, api_id, api_hash) as client: 
+
+    for message in client.iter_messages(chat): 
+        user = client.get_entity(message.sender_id) 
+        
+        print(f'Имя отправителя : {user.username}, Id отправителя {message.sender_id}, Сообщение : {message.text}')
