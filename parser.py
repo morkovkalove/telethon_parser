898ba04b575f51391e14824d2dfc2cd365732024
Andrew <73894426+morkovkalove@users.noreply.github.com>
2022-09-09 14:50:54 +0300

    Update parser.py

--- parser.py
+++ parser.py
@@ -4,13 +4,23 @@ from telethon.tl.functions.messages import GetHistoryRequest
 api_id = 12345  #your api id
 api_hash = 'asdfghjkl123' #your api hash 
 name = 'parsing_session' #any name
-chat = 'https://t.me/...'  # Канал откуда парсить
+chat = 'chanell_name'  # Канал откуда парсить
 
 
-with TelegramClient(name, api_id, api_hash) as client: 
 
-    for message in client.iter_messages(chat): 
-        user = client.get_entity(message.sender_id) 
+client = TelegramClient(name, api_id, api_hash)
+# updates = client(ImportChatInviteRequest('https://t.me/+OflQ5VeKI_ljNzNi'))
+# chat = client.get_entity('1667947415')
+
+
+@client.on(events.NewMessage(chats=chat))
+async def startingMessages(event):
+    #Выводим по 1 сообщению в консоль в режиме реального времени
+    async for event.message in client.iter_messages(chat, limit=1):
+        user = await client.get_entity(event.message.sender_id)
         message_dt = datetime.datetime.now()
-        print(f'Когда: {message_dt}  Имя отправителя : {user.username}, Id отправителя {message.sender_id}, Сообщение : {message.text}')
+        print(f'Когда: {message_dt:%d.%m.%Y %H:%M}  Имя отправителя : {user.username}, Id отправителя {event.message.sender_id}, Сообщение : {event.message.text}')
+      
 
+client.start()
+client.run_until_disconnected()
